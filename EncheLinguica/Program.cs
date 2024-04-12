e3835894b5723fede9e2f48147f26e100187b2d4
Lux Theris <lux.ray10.01@gmail.com>
2024-04-11 21:00:04 -0300

    Atualizando algumas coisas

--- EncheLinguica/Program.cs
+++ EncheLinguica/Program.cs
@@ -1,19 +1,11 @@
-﻿namespace program;
-{
-  class program
+﻿while (true)
   {
-    statis void Main(string[] args)
-    {
-      Console.WriteLine("Olá, seja bem vindo à este programa inutil. Ele é super inutil, sabia? Ele serve apenas para encher linguiça neste fork. Tipo, eu nem sei o que fazer direito nisto aqui...\n Acho que vou só criar um programa com um monte de texto para encher um pouco de linguiça e então pedir para você ler esta mensagem novamente até você se cansar e encerrar o programa manualmente.... É, é isso mesmo que eu vou fazer, ou pelo menos tentar");
-      Console.WriteLine("\n\nQuer continuar o programa? S/N: ");
-      let FimPrograma = Console.ReadLine();
-      if (FimPrograma == "S")
-      {
-        program;
-      }else
-      {
-        Console.WriteLine("Adios!");
-      }
-    }
+  Console.WriteLine("Olá, seja bem vindo à este programa inutil. Ele é super inutil, sabia? Ele serve apenas para encher linguiça neste fork. Tipo, eu nem sei o que fazer direito nisto aqui...\n Acho que vou só criar um programa com um monte de texto para encher um pouco de linguiça e então pedir para você ler esta mensagem novamente até você se cansar e encerrar o programa manualmente.... É, é isso mesmo que eu vou fazer, ou pelo menos tentar");
+  Console.WriteLine("\n\nQuer continuar o programa? S/N: ");
+  let FimPrograma = Console.ReadLine();
+  if (FimPrograma == "N")
+  {
+    Console.WriteLine("Adios!");
+    break;
   }
 }
